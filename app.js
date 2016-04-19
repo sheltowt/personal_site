b5f7bd0848ad72b691582a1418e5306412553e34
sheltowt <sheltowt@gmail.com>
2016-04-19 11:57:10 -0700

    adding a bit more

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get('/home', function (req, res) {
 	res.sendFile(path.join(__dirname, 'public/html', 'index.html'));
 })
 
+app.get('/about', function (req, res) {
+	res.sendFile(path.join(__dirname, 'public/html', 'about.html'));
+})
+
+app.get('/contact', function (req, res) {
+	res.sendFile(path.join(__dirname, 'public/html', 'contact.html'));
+})
+
 app.listen(3000, function() {
 	console.log('listening on port 3000!')
 })
